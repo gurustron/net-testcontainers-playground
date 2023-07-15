934aa8f64ad17d0f69a662c13d6950fd860a6f96
gurustron <gurustronpublic@gmail.com>
2023-07-15 20:26:43 +0300

    test in container

--- PlayTestContainers.Tests/Tests.cs
+++ PlayTestContainers.Tests/Tests.cs
@@ -1,15 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using PlayTestContainers.Data;
+using PlayTestContainers.Data.Model;
+using Testcontainers.PostgreSql;
+
 namespace PlayTestContainers.Tests;
 
 public class Tests
 {
-    [SetUp]
-    public void Setup()
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+        .WithImage("postgres:15-alpine")
+        .Build();
+
+    [OneTimeSetUp]
+    public async Task Setup()
     {
+        await _postgres.StartAsync();
+        var options = new DbContextOptionsBuilder<TestContainersAppContext>()
+            .UseNpgsql(_postgres.GetConnectionString())
+            .Options;
+        await using var ctx = new TestContainersAppContext(options);
+        await ctx.Database.EnsureCreatedAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task TearDown()
+    {
+        await _postgres.StartAsync();
     }
 
     [Test]
-    public void Test1()
+    public async Task Test1()
     {
-        Assert.Pass();
+        var options = new DbContextOptionsBuilder<TestContainersAppContext>()
+            .UseNpgsql(_postgres.GetConnectionString())
+            .Options;
+
+        using (var ctx = new TestContainersAppContext(options))
+        {
+            ctx.Enities.Add(new SomeEnity
+            {
+                SomeValue = "Test2"
+            });
+            await ctx.SaveChangesAsync();
+        }
+
+        using (var ctx = new TestContainersAppContext(options))
+        {
+            Assert.That(await ctx.Enities.CountAsync(), Is.EqualTo(1));
+        }
     }
 }
