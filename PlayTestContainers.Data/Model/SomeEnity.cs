c76155842580ec8a70ac4aa54eaf728b9f68f7b6
gurustron <gurustronpublic@gmail.com>
2023-07-24 21:00:03 +0300

    default value

--- PlayTestContainers.Data/Model/SomeEnity.cs
+++ PlayTestContainers.Data/Model/SomeEnity.cs
@@ -3,5 +3,5 @@ namespace PlayTestContainers.Data.Model;
 public class SomeEnity
 {
     public int Id { get; set; }
-    public string SomeValue { get; set; }
+    public string SomeValue { get; set; } = "";
 }
